cebe554c6909e53d96b9fd1be006b576c341606e
Chris Chappell <cr.chappell@pm.me>
2024-04-29 18:53:21 -0400

    3rd commit

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 # Day 1 of of 100 Days of Code Python
 # Practice using GIT on the 2nd try through this course
+# Band name generator.
 
+print("Hello, welcome to the band  name generator")
+
+city_name = input("What city did you grow up in?\n")
+pet_name = input("What was the name of your favorite childhood pet>\n")
+
+print("The name of your band is " + city_name + " " + pet_name)
+
+# Solution located at: https://replit.com/@appbrewery/band-name-generator-end
